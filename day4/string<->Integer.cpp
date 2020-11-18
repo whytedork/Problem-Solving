13abf62d274a9b92e3a341a5dbf44cf883314bf3
Hemant Gangwar <34183672+whytedork@users.noreply.github.com>
2020-11-18 08:20:02 +0530

    strin to int and vice versa

---
+++ day4/string<->Integer.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main() {
+	string s1;
+	int i1;
+
+	//string to int
+	cout << "Enter numeric string:";
+	cin >> s1;
+	cout << stoi(s1) << endl;
+
+	//integer to string
+	cout << "Enter Integer :";
+	cin >> i1;
+	cout << to_string(i1) << endl;
+
+	return 0;
+}
